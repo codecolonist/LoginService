d692e30b9254af3d697e5a15acee84aed3b1345c
Ramkishorerao Gujjeti <codecolonist@gmail.com>
2019-04-27 23:10:30 -0500

    Login service initial commit

---
+++ src/main/java/com/fedex/LoginService/bean/RegistrationRequest.java
@@ -0,0 +1,18 @@
+package com.fedex.LoginService.bean;
+
+import com.fedex.LoginService.Repository.UserInfo;
+
+public class RegistrationRequest {
+	
+	private UserInfo mpuser;
+
+	public UserInfo getMpuser() {
+		return mpuser;
+	}
+
+	public void setMpuser(UserInfo mpuser) {
+		this.mpuser = mpuser;
+	}
+	
+
+}
